1c8a8e5b918290e07257a959d2650358c41f717a
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2024-11-28 23:37:45 +0100

    added router and part of an example

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom'
+import Root from './routes/root.tsx'
+import ErrorPage from './error-page.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: < Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
