f91bd93f14ba1909a2e1ef740e51dd4a7017c0b0
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2024-11-29 18:06:25 +0100

    added mui for widgets

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,12 +1,12 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
-import Root from './routes/root.tsx'
-import ErrorPage from './error-page.tsx'
+import Root from './routes/root'
+import ErrorPage from './error-page'
 
 const router = createBrowserRouter([
   {
