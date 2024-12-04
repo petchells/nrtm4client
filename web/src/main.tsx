9443e387bb7bf4b47a1345ca1733cdca4b1af804
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2024-12-04 13:52:51 +0100

    mui dashboard example works

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,23 +1,18 @@
-import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {
-  createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
-import Root from './routes/root'
-import ErrorPage from './error-page'
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: < Root />,
-    errorElement: <ErrorPage />,
-  },
-]);
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from './theme'
+import { router } from './routes/root'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   </StrictMode>,
 )
