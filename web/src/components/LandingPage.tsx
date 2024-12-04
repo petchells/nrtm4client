c26c01876959f43cdbab288cf905ddf6d2e0f9da
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2024-12-04 14:35:04 +0100

    formatting

--- web/src/components/LandingPage.tsx
+++ web/src/components/LandingPage.tsx
@@ -12,51 +12,51 @@ import MainGrid from './MainGrid';
 import SideMenu from './SideMenu';
 import AppTheme from './shared-theme/AppTheme';
 import {
-	chartsCustomizations,
-	dataGridCustomizations,
-	datePickersCustomizations,
-	treeViewCustomizations,
+    chartsCustomizations,
+    dataGridCustomizations,
+    datePickersCustomizations,
+    treeViewCustomizations,
 } from '../theme/customizations';
 
 const xThemeComponents = {
-	...chartsCustomizations,
-	...dataGridCustomizations,
-	...datePickersCustomizations,
-	...treeViewCustomizations,
+    ...chartsCustomizations,
+    ...dataGridCustomizations,
+    ...datePickersCustomizations,
+    ...treeViewCustomizations,
 };
 
 export default function LandingPage(props: { disableCustomTheme?: boolean }) {
-	return (
-		<AppTheme {...props} themeComponents={xThemeComponents}>
-			<CssBaseline enableColorScheme />
-			<Box sx={{ display: 'flex' }}>
-				<SideMenu />
-				<AppNavbar />
-				{/* Main content */}
-				<Box
-					component="main"
-					sx={(theme) => ({
-						flexGrow: 1,
-						backgroundColor: theme.vars
-							? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
-							: alpha(theme.palette.background.default, 1),
-						overflow: 'auto',
-					})}
-				>
-					<Stack
-						spacing={2}
-						sx={{
-							alignItems: 'center',
-							mx: 3,
-							pb: 5,
-							mt: { xs: 8, md: 0 },
-						}}
-					>
-						<Header />
-						<MainGrid />
-					</Stack>
-				</Box>
-			</Box>
-		</AppTheme>
-	);
+    return (
+        <AppTheme {...props} themeComponents={xThemeComponents}>
+            <CssBaseline enableColorScheme />
+            <Box sx={{ display: 'flex' }}>
+                <SideMenu />
+                <AppNavbar />
+                {/* Main content */}
+                <Box
+                    component="main"
+                    sx={(theme) => ({
+                        flexGrow: 1,
+                        backgroundColor: theme.vars
+                            ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+                            : alpha(theme.palette.background.default, 1),
+                        overflow: 'auto',
+                    })}
+                >
+                    <Stack
+                        spacing={2}
+                        sx={{
+                            alignItems: 'center',
+                            mx: 3,
+                            pb: 5,
+                            mt: { xs: 8, md: 0 },
+                        }}
+                    >
+                        <Header />
+                        <MainGrid />
+                    </Stack>
+                </Box>
+            </Box>
+        </AppTheme>
+    );
 }
