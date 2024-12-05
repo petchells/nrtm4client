7d786a796728e62afb47a2059ad1eff157887ff8
Paul Etchells (interceptor) <p.a.etchells@gmail.com>
2024-12-05 23:00:00 +0100

    api serves a single rpc method

--- internal/nrtm4serve/launcher.go
+++ internal/nrtm4serve/launcher.go
@@ -1,17 +1,24 @@
 package nrtm4serve
 
 import (
+	"log"
 	"net/http"
 	"time"
 
+	"gitlab.com/etchells/nrtm4client/internal/nrtm4/pg"
 	"gitlab.com/etchells/nrtm4client/internal/nrtm4/service"
 	"gitlab.com/etchells/nrtm4client/internal/nrtm4serve/rpc"
 )
 
 // Launch sets up the rpc handler and starts the server
 func Launch(config service.AppConfig, port int, webDir string) {
-	rpcHandler := rpc.Handler{API: WebAPI{}}
-	logger.Info("Cashdash api server starting", "port", port)
+	repo := pg.PostgresRepository{}
+	if err := repo.Initialize(config.PgDatabaseURL); err != nil {
+		log.Fatal("Failed to initialize repository")
+	}
+	defer repo.Close()
+	rpcHandler := rpc.Handler{API: WebAPI{Repo: repo}}
+	logger.Info("NRTM4serve is starting", "port", port)
 	defer func() {
 		if r := recover(); r != nil {
 			logger.Error("Recovered from Panic in launcher", "recover", r)
--- internal/nrtm4serve/webapi.go
+++ internal/nrtm4serve/webapi.go
@@ -1,6 +1,8 @@
 package nrtm4serve
 
 import (
+	"net/http"
+
 	"gitlab.com/etchells/nrtm4client/internal/nrtm4/persist"
 	"gitlab.com/etchells/nrtm4client/internal/nrtm4serve/rpc"
 )
@@ -8,9 +10,15 @@ import (
 // WebAPI defines the RPC functions used by the web client
 type WebAPI struct {
 	rpc.API
-	repo persist.Repository
+	Repo persist.Repository
 }
 
+// GetAuth implements interface -- allows requests to all methods
+func (api WebAPI) GetAuth(w http.ResponseWriter, r *http.Request, req rpc.JSONRPCRequest) (rpc.WebSession, bool) {
+	return rpc.WebSession{}, true
+}
+
+// GetSources returns a list of sources
 func (api WebAPI) GetSources() ([]persist.NRTMSource, error) {
-	return api.repo.GetSources()
+	return api.Repo.GetSources()
 }
